266d74406bd52b9fd472a437e69cdfa63e8c880e
kdharmendra <technicalradhe@gmail.com>
2024-07-17 17:23:10 +0530

    todo model using mongoose

--- mongoose/models/todo/user.models.js
+++ mongoose/models/todo/user.models.js
@@ -5,10 +5,14 @@ const userSchema = new mongoose.Schema(
         username: {
             type: String,
             required: true,
+            unique: true,
+            lowercase: true,
         },
         email: {
             type: String,
             required: true,
+            unique: true,
+            lowercase: true,
         },
         password: {
             type: String,
